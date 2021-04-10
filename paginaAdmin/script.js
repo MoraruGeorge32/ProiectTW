fea23cf282da00431bf55df003c9187bac1af190
RiZZeR <ramusteata@gmail.com>
2021-04-10 23:26:45 +0300

    stilizari pagina administrare
    stilizare pagina administrare:
    crearea unei interfete mai atractive pentru selctarea actiunilor ce le poate executa un administrator
    folosirea unui script scris in javascript pentru a arata continutului specific unui buton
    resolvarea aproximativa a responsivness-ului

---
+++ paginaAdmin/script.js
@@ -0,0 +1,9 @@
+function arataAdaugaEveniment(){
+    document.getElementById('removeEvent').style.display="none";
+    document.getElementById('formAddEvent').style.display="block";
+}
+
+function arataStergeEveniment(){
+    document.getElementById('formAddEvent').style.display="none";
+    document.getElementById('removeEvent').style.display="block";
+}
