6110b5c4bc655312adc8b12de9e8ac45b3956fcb
Vlad Anghelus <anghelus.vlad30@gmail.com>
2021-05-26 15:48:54 +0300

    Afisare lista evenimente pentru stergere

--- mvc/app/views/paginaAdmin/lista_evenimente.php
+++ mvc/app/views/paginaAdmin/lista_evenimente.php
@@ -1,14 +1,20 @@
 <?php
-    class ListaEvenimente(){
-        public function __construct(){
-            $this->parse();
-        }
-        public function parse(){
-            if(isset($_GET['start']&&)&&isset($_GET['counter'])){
-                echo "<script>console.log("."bravo!!".")</script>";
-            }
-        }
-    } 
-    //new ListaEvenimente();
-    echo $_GET['start'].$_GET['counter'];
+    
+    $dbconn = new mysqli("localhost", "Robert", "robert", "globalterrorismdb");
+    if($dbconn->connect_error) {
+        exit('Could not connect');
+      }
+    $sql = "SELECT eventid, iyear, imonth, iday, country_txt FROM globalterrorismdb_0718dist ORDER BY eventid LIMIT ?";
+    $stmt = $dbconn->prepare($sql);
+    $stmt->bind_param("i", $_GET['counter']);
+    $stmt->execute();
+   // $stmt->store_result();
+   // $stmt->bind_result($evid, $an, $luna, $zi, $tara);
+   // while( $stmt->fetch() ){
+   //     echo htmlspecialchars($evid . " " . $an . " " . $luna . " " . $zi . " " . $tara . "\n");
+   // }
+    $result = $stmt->get_result();
+    $out = $result->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
+    echo json_encode($out);
 ?>
